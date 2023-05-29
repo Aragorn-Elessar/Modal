49faf9e71fe9c1abf1d8119a1a06cb20e27fbbd8
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2023-05-29 18:01:17 +0300

    Add descriptive comments to main functions

--- js/script.js
+++ js/script.js
@@ -27,12 +27,17 @@ const closeModal = function () {
  * Main Functions
  *
  */
+// Show modal window on button click
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
+// Close modal window on X button click
 btnCloseModal.addEventListener('click', closeModal);
+
+// Close modal on overlay click
 overlay.addEventListener('click', closeModal);
 
+// Close modal on Esc key down
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape' && !modal.classList.contains('hidden'))
     closeModal();
